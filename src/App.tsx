3a47efc364ceb4f74c4355db5fffeafa48f454c3
김성찬 <tjdcksdl00@naver.com>
2022-10-23 00:20:49 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+    // let myNumber: number = '23'; // 정의된 타입에 맞는 데이터가 아니어서 에러 발생
+
+    return (
+        <div>
+            <p>Hello, React!</p>
+            <p>Hello, Typescript!</p>
+        </div>
+    );
+};
+
+export default App;
