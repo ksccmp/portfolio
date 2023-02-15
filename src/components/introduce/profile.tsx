faa2f4b8e0e77d470797448c9447ee76ea167e6f
김성찬 <tjdcksdl00@naver.com>
2023-02-15 14:23:57 +0900

    profile listItemFormControl 적용

--- src/components/introduce/profile.tsx
+++ src/components/introduce/profile.tsx
@@ -4,9 +4,8 @@ import { getImage } from '@src/utils/getImage';
 
 import Image from '../atoms/image';
 import List from '../atoms/list';
-import ListItem from '../atoms/listItem';
-import Text from '../atoms/text';
 import Title from '../atoms/title';
+import ListItemFormControl from '../molecules/introduce/listItemFormControl';
 
 const Profile = () => {
     return (
@@ -22,23 +21,9 @@ const Profile = () => {
                     </Title>
 
                     <List listStyleType="disc">
-                        <TextListItem>
-                            <Text color="grayscale60" size="large">
-                                안녕하세요! FE 개발자로 꾸준하고 가파르게 성장하는 것을 목표로 하고 있는 김성찬입니다.
-                            </Text>
-                        </TextListItem>
-
-                        <TextListItem>
-                            <Text color="grayscale60" size="large">
-                                단 한줄의 코드도 다른 사람들이 이해할 수 있는 코드를 작성하려고 합니다.
-                            </Text>
-                        </TextListItem>
-
-                        <TextListItem>
-                            <Text color="grayscale60" size="large">
-                                혼자하는 것보다 협업하면서 개발하는 것을 즐깁니다.
-                            </Text>
-                        </TextListItem>
+                        <ListItemFormControl label="안녕하세요! FE 개발자로 꾸준하고 가파르게 성장하는 것을 목표로 하고 있는 김성찬입니다." />
+                        <ListItemFormControl label="단 한줄의 코드도 다른 사람들이 이해할 수 있는 코드를 작성하려고 합니다." />
+                        <ListItemFormControl label="혼자하는 것보다 협업하면서 개발하는 것을 즐깁니다." />
                     </List>
                 </IntroduceHeader>
             </IntroduceSection>
@@ -61,10 +46,4 @@ const IntroduceHeader = styled.header`
     display: flex;
     flex-direction: column;
     gap: 8px;
-`;
-
-const TextListItem = styled(ListItem)`
-    & > p {
-        display: inline;
-    }
 `;
