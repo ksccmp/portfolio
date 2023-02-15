0604dade0ddd516eba5b84a953f743390ccb688d
김성찬 <tjdcksdl00@naver.com>
2023-02-15 10:06:18 +0900

    배경이미지 삭제

--- src/utils/getImage.ts
+++ src/utils/getImage.ts
@@ -1,9 +1,9 @@
-type Src = 'BACKGROUND';
+type Src = 'PROFILE';
 
 export const getImage = (src: Src) => {
     switch (src) {
-        case 'BACKGROUND':
-            return '/images/background.jpg';
+        case 'PROFILE':
+            return '/images/profile.jpg';
 
         default:
             return '';
